1e97b81987100855af0951f87f4b49d59fd46fa5
Priyanshu <pv254424@gmail.com>
2026-04-07 00:35:07 +0530

    saveReading made with service

--- TrafficService/TrafficService/src/main/java/com/smartCity/TrafficService/repository/TrafficRepository.java
+++ TrafficService/TrafficService/src/main/java/com/smartCity/TrafficService/repository/TrafficRepository.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface TrafficRepository extends JpaRepository<TrafficRepository, Long> {
+public interface TrafficRepository extends JpaRepository<TrafficReading, Long> {
 
     // Latest reading for each sensor
     @Query("SELECT t FROM TrafficReading t WHERE t.timestamp = " +
--- TrafficService/TrafficService/src/main/java/com/smartCity/TrafficService/service/TrafficService.java
+++ TrafficService/TrafficService/src/main/java/com/smartCity/TrafficService/service/TrafficService.java
@@ -1,8 +1,12 @@
 package com.smartCity.TrafficService.service;
 
+import com.smartCity.TrafficService.dto.TrafficDto;
+import com.smartCity.TrafficService.entity.TrafficReading;
 import com.smartCity.TrafficService.repository.TrafficRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
@@ -23,6 +27,25 @@ public class TrafficService {
                 t.setDaemon(true);
                 return t;
             });
+
+    @Transactional
+    @CacheEvict(value = {"traffic-summary", "critical-zones"} , allEntries = true)
+    public void saveReading(TrafficDto.TrafficSensorData data){
+        TrafficReading reading = TrafficReading.builder()
+                .sensorId(data.getSensorId())
+                .location(data.getLocation())
+                .vehicleCount(data.getVehicleCount())
+                .avgSpeed(data.getAvgSpeed())
+                .congestionLevel(data.getCongestionLevel())
+                .timestamp(data.getTimestamp())
+                .build();
+
+        TrafficReading saved = trafficRepository.save(reading);
+        log.info("Saved traffic reading id={} for {}" , saved.getId(), data.getLocation());
+
+        if("CRITICAL".equals(data.getCongestionLevel())){}
+
+    }
 
 
 }
