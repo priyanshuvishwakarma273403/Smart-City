222518d2e40ddaf456c144a5f0e5703dda91e7d2
Priyanshu <pv254424@gmail.com>
2026-04-06 15:59:01 +0530

    fixed evry 3 second traffic data update

---
+++ SensorSimulator/SensorSimulator/src/main/java/com/smartCity/SensorSimulator/simulator/SensorDataSimulator.java
@@ -0,0 +1,52 @@
+package com.smartCity.SensorSimulator.simulator;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class SensorDataSimulator {
+
+    private final KafkaTemplate<String, Object> kafkaTemplate;
+    private final Random random = new Random();
+
+    private final ExecutorService sensorThreadPool =
+            Executors.newFixedThreadPool(3, r -> {
+                Thread t = new Thread(r);
+                t.setName("sensorThread-" + t.getId());
+                t.setDaemon(true);
+                return t;
+            });
+
+    private static final List<String> TRAFFIC_LOCATIONS = Arrays.asList(
+            "Connaught Place", "Karol Bagh", "Lajpat Nagar",
+            "Dwarka Sector 21", "Nehru Place", "Chandni Chowk"
+    );
+
+    private static final List<String> AIR_LOCATIONS = Arrays.asList(
+            "Anand Vihar", "RK Puram", "Punjabi Bagh",
+            "Jahangirpuri", "Okhla", "Rohini"
+    );
+
+    private static final String[] SLOT_NUMBERS = {"01", "02", "03", "04", "05"};
+
+
+    @Scheduled(fixedDelay = 3000)
+    public void simulateTrafficSensors(){
+
+
+    }
+
+
+}
