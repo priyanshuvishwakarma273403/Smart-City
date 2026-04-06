23c4d79ab268a57d98014d28a60b6eadce33f2ee
Priyanshu <pv254424@gmail.com>
2026-04-06 15:08:51 +0530

    init the sensor simulator

---
+++ SensorSimulator/SensorSimulator/src/test/java/com/smartCity/SensorSimulator/SensorSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartCity.SensorSimulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensorSimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
