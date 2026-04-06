4cc7e4ddd683a4bbcae1c2c64f32864e0e5dab4b
Priyanshu <pv254424@gmail.com>
2026-04-07 00:02:14 +0530

    traffic reading in entity complete

--- TrafficService/TrafficService/src/main/java/com/smartCity/TrafficService/entity/TrafficReading.java
+++ TrafficService/TrafficService/src/main/java/com/smartCity/TrafficService/entity/TrafficReading.java
@@ -1,10 +1,43 @@
 package com.smartCity.TrafficService.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "traffic_reading")
+@Table(name = "traffic_reading",
+indexes = @Index(columnList = "sensorId, timestamp"))
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class TrafficReading {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String sensorId;
+
+    @Column(nullable = false, length = 100)
+    private String location;
+
+    @Column(nullable = false)
+    private int vehicleCount;
+
+    @Column(nullable = false)
+    private double avgSpeed;
+
+    @Column(nullable = false, length = 20)
+    private String congestionLevel;
+
+    @Column(nullable = false)
+    private LocalDateTime timestamp;
+
 
 }
