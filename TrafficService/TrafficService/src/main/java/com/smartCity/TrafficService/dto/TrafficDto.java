705fb174547214ac620cfa280d472845631f8f2b
Priyanshu <pv254424@gmail.com>
2026-04-07 00:13:52 +0530

    traffic reading response completed

--- TrafficService/TrafficService/src/main/java/com/smartCity/TrafficService/dto/TrafficDto.java
+++ TrafficService/TrafficService/src/main/java/com/smartCity/TrafficService/dto/TrafficDto.java
@@ -1,11 +1,19 @@
 package com.smartCity.TrafficService.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 public class TrafficDto {
 
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
     public static class TrafficSensorData {
         private String sensorId;
         private String location;
@@ -16,4 +24,19 @@ public class TrafficDto {
         @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
         private LocalDateTime timestamp;
     }
+
+    public static class TrafficReadingResponse{
+        private Long id;
+        private String sensorId;
+        private String location;
+        private int vehicleCount;
+        private double avgSpeed;
+        private String congestionLevel;
+
+        @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+        private LocalDateTime timestamp;
+    }
+
+
+
 }
